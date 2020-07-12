01e804745f9b47737ed43e46e6e3adee34b055a1
tesso <ta4780@icloud.com>
2020-07-12 17:03:56 +0900

    webpack.config.jsを作成した。とりあえずwebpack-dev-serverは動きそう

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import * as lib from './app.js';
-console.log(lib.square(11)); // 121
-console.log(lib.diag(4, 3)); // 5
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports ={
+    entry: `./src/js/index.js`,
+
+    output:{
+        path: `${__dirname}/dist`,
+        filename:"main.js"
+    },
+    //production -> 最適化(ビルドに時間かかるけどパフォーマンスいい感じ)
+    //development -> ソースマップ有効(容量がクソ多い)
+    mode:"development",
+    devServer: {
+        contentBase: `${__dirname}/`,//ホストする先
+        port: 8080,//ポート
+        open: true
+    }
+};
